9783f1c3eb1e7e54c495a91f782dbed0d98e9459
j4l13n <juliushirwa@gmail.com>
2019-06-09 20:03:14 +0200

    Add middleware to subscribers creation

--- middlewares/subscribers.js
+++ middlewares/subscribers.js
@@ -1,17 +1,22 @@
 import validator from 'validator';
-import validations from './../validations/validations';
 
 class SubscriberValidations {
     create(req, res, next) {
          const name = req.body.name || '';
          const phone = req.body.phone || '';
 
-         if(!validations.isValidName(name)) {
+         if(validator.isEmpty(name)) {
             res.json({
                 error: "Name field is required"
+            })
+         } else if(validator.isAlpha(name)) {
+            res.json({
+                error: "Name must contain only alphanumeric"
             });
-         } else if(validations) {
-
+         } else if(!validator.isMobilePhone(phone)) {
+            res.json({
+                error: "Phone must have twelve numbers example: 250788384548"
+            })
          } else {
              next();
          }
--- routes/subscribers.route.js
+++ routes/subscribers.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import subscibers from './../controllers/subscribers';
+import subscriberValidation from './../middlewares/subscribers';
 
 const router = express.Router();
 
 router.get("/subscribers", subscibers.getAll);
 router.get("/subscribers/:id", subscibers.getOne);
-router.post("/subscribers", subscibers.create);
+router.post("/subscribers", subscriberValidation.create, subscibers.create);
 router.put("/subscribers/:id", subscibers.update);
 
 export default router;
